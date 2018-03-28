8d6808465dfc6bf92bcf7f8d999b961f1a3955f6
hughe <hughechen@bluetechnology.com.tw>
2018-03-28 14:39:25 +0800

    fix model driver

--- models/driver.js
+++ models/driver.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const PointSchema = new Schema({
   type: {type: String, default: "Point"},
   coordinates: {type:[Number], index:'2dsphere'}
-});
+},{_id:false});
 
 const DriverSchema = new Schema({
   email:{
